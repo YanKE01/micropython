81e7fa5a07157604ecabc072a137972db8c3b3fe
yanke <yanke2@espressif.com>
2026-04-29 20:54:00 +0800

    feat: add esp32-p4x-eye

---
+++ ports/esp32/boards/ESP32_P4X_EYE/mpconfigboard.h
@@ -0,0 +1,30 @@
+#ifndef MICROPY_HW_BOARD_NAME
+#define MICROPY_HW_BOARD_NAME "ESP32-P4X-EYE"
+#endif
+
+#ifndef MICROPY_HW_MCU_NAME
+#define MICROPY_HW_MCU_NAME "ESP32P4"
+#endif
+
+#define MICROPY_HW_USB_MANUFACTURER_STRING "ESP-VISION"
+#define MICROPY_HW_USB_PRODUCT_FS_STRING "ESP32-P4X-EYE MicroPython"
+
+#define MICROPY_PY_ESPNOW                (0)
+
+#define MICROPY_HW_ENABLE_SDCARD         (1)
+
+#ifndef USB_SERIAL_JTAG_PACKET_SZ_BYTES
+#define USB_SERIAL_JTAG_PACKET_SZ_BYTES  (64)
+#endif
+
+#define MICROPY_HW_ENABLE_UART_REPL      (1)
+
+#define MICROPY_PY_MACHINE_I2S           (1)
+
+#ifndef MICROPY_PY_NETWORK_WLAN
+#define MICROPY_PY_NETWORK_WLAN          (0)
+#endif
+
+#ifndef MICROPY_PY_BLUETOOTH
+#define MICROPY_PY_BLUETOOTH             (0)
+#endif
